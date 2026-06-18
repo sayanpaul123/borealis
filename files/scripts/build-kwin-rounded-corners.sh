a67bfb16a823931589a49c8e31e1a433a58b2e20
Sayan Paul <sayanpaul0411@gmail.com>
2026-06-19 02:18:45 +0530

    install instead of build

--- files/scripts/build-kwin-rounded-corners.sh
+++ files/scripts/build-kwin-rounded-corners.sh
@@ -1,37 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Get the KWin version currently in the image
+KWIN_VER=$(rpm -q kwin-common --qf '%{version}')
+
+# Install the matching version from COPR
+dnf copr enable -y matinlotfali/KDE-Rounded-Corners
 dnf install -y --setopt=install_weak_deps=False \
-    git cmake gcc-c++ extra-cmake-modules \
-    kwin-devel \
-    kf6-kconfigwidgets-devel \
-    kf6-kcmutils-devel \
-    kf6-ki18n-devel \
-    qt6-qtbase-private-devel \
-    libepoxy-devel \
-    wayland-devel \
-    libdrm-devel
-
-git clone --depth 1 https://github.com/matinlotfali/KDE-Rounded-Corners /tmp/krc
-
-cmake -S /tmp/krc -B /tmp/krc/build \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DKDE_INSTALL_LIBDIR=lib64
-
-cmake --build /tmp/krc/build -j"$(nproc)"
-cmake --install /tmp/krc/build
-
-# clean up build deps and source
-dnf remove -y \
-    git cmake gcc-c++ extra-cmake-modules \
-    kwin-devel \
-    kf6-kconfigwidgets-devel \
-    kf6-kcmutils-devel \
-    kf6-ki18n-devel \
-    qt6-qtbase-private-devel \
-    libepoxy-devel \
-    wayland-devel \
-    libdrm-devel
-
-rm -rf /tmp/krc
+    "kwin-effect-roundcorners-*-${KWIN_VER}.*"
+dnf copr disable -y matinlotfali/KDE-Rounded-Corners
